b5ccc6472b462357e64b53afb95ce89b1167bd67
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-06-06 17:11:53 +0530

    Completed scenario6

---
+++ scenario6/src/app/component1/component1.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component implements OnInit {
+
+
+  public text ="";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  myEvent(value){
+    console.log(value);
+    this.text=value;
+  }
+
+}
